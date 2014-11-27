466e42d1b9a2a78abca7a51147a381e902c15956
Daoud Clarke <daoud.clarke@gmail.com>
2014-11-27 17:23:58 +0000

    Add a random baseline

--- kernelparse/baseline.py
+++ kernelparse/baseline.py
@@ -9,9 +9,10 @@ from analyse import analyse
 from kernelparse.experiment import Experiment
 from kernelparse.tensor import TensorParser
 from kernelparse.oracle import OracleParser
+from kernelparse.randomparser import RandomParser
 
 PARSERS = {repr(parser): parser for parser in 
-           [TensorParser(), OracleParser()] }
+           [TensorParser(), OracleParser(), RandomParser()] }
 
 def output_results(results, results_path):
     with open(results_path, 'w') as results_file:
@@ -19,13 +20,12 @@ def output_results(results, results_path):
 
 
 def run(dataset_path, results_path, parser_names):
+    parsers = [PARSERS[name] for name in parser_names]
     with open(dataset_path) as dataset_file:
-        dataset = islice(dataset_file, 10000)
-        dataset = [json.loads(row) for row in dataset if len(row.strip()) > 0]
+        dataset = [json.loads(row) for row in dataset_file if len(row.strip()) > 0]
 
     experiment = Experiment()
     
-    parsers = [PARSERS[name] for name in parser_names]
     results = experiment.run(dataset, parsers)
     output_results(results, results_path)
     print analyse(results)
@@ -38,9 +38,6 @@ if __name__ == "__main__":
     argument_parser.add_argument('dataset_path', type=str, help='Path to .json file containing the dataset')
     argument_parser.add_argument('results_path', type=str, help='Path in which to save the results in .json format')
     argument_parser.add_argument('parser_names', type=str, nargs='+', help='Which parser. Valid parsers are %s' % ', '.join(PARSERS))
-
-    #dataset_path = '/home/dc/Experiments/sempre-paraphrase-dataset/examples.json'
-    #results_path = 'results/results.json'
 
     args = argument_parser.parse_args()
     run(**vars(args))
