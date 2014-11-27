f457578ba2d06346c2cf775836ddc40dd3743fb1
Daoud Clarke <daoud.clarke@gmail.com>
2014-11-27 13:37:29 +0000

    Add missing files

---
+++ kernelparse/experiment.py
@@ -0,0 +1,40 @@
+from itertools import groupby
+from operator import itemgetter
+from random import Random
+
+from kernelparse.log import logger
+
+
+class Experiment(object):
+    """
+    Class to run experiments on Sempre paraphrase datasets.
+    """
+
+    def run(self, dataset, parser):
+        logger.info("Loading dataset")
+        random = Random(1)        
+
+        dataset = [(source, list(group)) for source, group in
+                    groupby(dataset, itemgetter('source'))]
+
+        if len(dataset) != len(set(map(itemgetter(0), dataset))):
+            raise ValueError("Dataset contains non-contiguous source examples")
+
+        # Shuffle the dataset and each group within the dataset
+        random.shuffle(dataset)
+        for source, group in dataset:
+            random.shuffle(group)
+
+        length = len(dataset)
+        assert length > 1
+
+        train_length = int(0.8*length)
+        train_set = dataset[:train_length]
+        test_set = dataset[train_length:]
+
+        parser.train(train_set)
+        return parser.test(test_set)
+        
+        
+
+
